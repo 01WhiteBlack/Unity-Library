a8017a326e6798ec5d462ed04965b25fb43014ab
Wang <61880387+01WhiteBlack@users.noreply.github.com>
2023-02-13 18:13:27 +0800

    Delete Scripts directory

--- Scripts/EventManager/OpenWinFolder/FolderDialog/FilterFileParameters.cs
+++
@@ -1,9 +0,0 @@
-public class FilterFileParameters
-{
-    public bool iSMultiSelect = false;//是否多选
-    public string WindowTitle = "Open Folder Slecet File";//窗口标题
-    public string firstOpenPath = "D:\\";//首次使用的路径
-    public int filterIndex = 1;
-    public bool RestoreDirectory = false;
-    public string SelectFileType;//筛选的文件类型
-}
